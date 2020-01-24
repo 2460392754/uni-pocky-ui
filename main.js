f87e00a9665272d9a3a02b8ca2beb5b87294cc78
2460392754 <2460392754@qq.com>
2020-01-24 19:37:55 +0800

    fix：修改了 preview项目中小程序组件组件注册问题

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 import Vue from 'vue';
 import App from './App';
-import UniPockyUi from './src';
+// import UniPockyUi from './src';
 import './src/styles/index.scss';
 
-Vue.use(UniPockyUi);
+// Vue.use(UniPockyUi);
+
+import PButton from './src/components/p-button.vue';
+import PCard from './src/components/p-card.vue';
+import PNav from './src/components/p-nav.vue';
+import PModal from './src/components/p-modal.vue';
+import PIcon from './src/components/p-icon.vue';
+import PInput from './src/components/p-input.vue';
+import PLabel from './src/components/p-label.vue';
+import PTextarea from './src/components/p-textarea.vue';
+import PCheckbox from './src/components/p-checkbox.vue';
+import PSwitch from './src/components/p-switch.vue';
+
+Vue.component('PButton', PButton);
+Vue.component('PCard', PCard);
+Vue.component('PNav', PNav);
+Vue.component('PModal', PModal);
+Vue.component('PIcon', PIcon);
+Vue.component('PInput', PInput);
+Vue.component('PLabel', PLabel);
+Vue.component('PTextarea', PTextarea);
+Vue.component('PCheckbox', PCheckbox);
+Vue.component('PSwitch', PSwitch);
+
 Vue.config.productionTip = false;
 App.mpType = 'app';
 
