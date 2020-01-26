0663571a6ffa9cf5bb84f63809aca27b477f9731
2460392754 <2460392754@qq.com>
2020-01-26 23:01:50 +0800

    update

--- docs/config/index.js
+++ docs/config/index.js
@@ -1,4 +1,4 @@
 export const PREVIEW_BASH =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8080/#/preview/"
-        : "https://npm.plugin.pocky.top/uni-pocky-ui/preview/#/";
+        : "https://npm.plugin.pocky.top/uni-pocky-ui/preview/#/preview/";
